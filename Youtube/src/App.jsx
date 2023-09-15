0d5a812ce564dd3cf124080883b548ae513d45f2
Sagar Darade <daradesagar.1998@gmail.com>
2023-09-15 13:40:00 +0530

    Added react icons

--- Youtube/src/App.jsx
+++ Youtube/src/App.jsx
@@ -1,7 +1,9 @@
+import { FaYoutube } from "react-icons/fa";
+
 function App() {
 	return (
 		<h1 className="text-3xl text-red-500 font-bold underline">
-			Hello world!
+			<FaYoutube />
 		</h1>
 	);
 }
